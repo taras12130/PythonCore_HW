d610255669bd9ecab21a084a0b870667b42158b6
Taras Kniazhyk <taras12130@gmail.com>
2020-12-07 21:26:46 +0200

    made some changes

--- homework_4/task_2.py
+++ homework_4/task_2.py
@@ -1,12 +1,9 @@
 # Create a list that contains elements of integer type,
 # then use the loop to change the type of these elements to a floating type. 
 try:
-    list_of_int_numbers = []
+    list_of_int_numbers = [int(input(f"Enter a {i + 1} number: ")) for i in range(int(input("How many numbers in your list? ")))]
     list_of_float_numbers = []
-    n = int(input("How many numbers in your list? "))
-    for i in range(n):
-        list_of_int_numbers.append(int(input("Enter a number: ")))
-    print(f"Your list is: {list_of_int_numbers}")
+    print(f"Your list is {list_of_int_numbers}")
     for i in list_of_int_numbers:
         list_of_float_numbers.append(float(i))
     print(f"Your list with float numbers is: {list_of_float_numbers}")
